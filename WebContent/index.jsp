8f76255181ff2c4af9a93916f9e31bb31bf09f2c
KevinZhangwy <2066427779@qq.com>
2020-07-09 19:56:25 +0800

    51shop

---
+++ WebContent/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>51商城</title>
+<style type="text/css">
+body{
+    magin:50px auto;    /*设置外边距*/
+    text-align: center;
+}
+</style>
+</head>
+<body>
+欢迎访问51商城
+<br>
+<img alt="网站正在建设中......" src="images/building.png">
+</body>
+</html>
---
+++ src/com/model/Member.java
@@ -0,0 +1,76 @@
+package com.model;
+
+public class Member {
+    private Integer ID=Integer.valueOf("-1");
+    private String username="";    
+    private String truename="";    //真实姓名
+    private String pwd ="";        //密码属性
+    private String city ="";       //所在城市
+    private String address ="";    //地址属性
+    private String postcode ="";   //邮编属性
+    private String cardno ="";     //证件
+    private String cardtype ="";   //证件类型
+    private String tel ="";        //联系电话
+    private String email="";       //邮箱属性
+	public Integer getID() {
+		return ID;
+	}
+	public void setID(Integer iD) {
+		ID = iD;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	public String getCardno() {
+		return cardno;
+	}
+	public void setCardno(String cardno) {
+		this.cardno = cardno;
+	}
+	public String getCardtype() {
+		return cardtype;
+	}
+	public void setCardtype(String cardtype) {
+		this.cardtype = cardtype;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
